06a7f4f33aab3d00e26946a9a3f2f6fbf4949d5f
fedorrychkov <fedorrychkov@yandex.ru>
2018-10-13 22:41:48 +0300

    add admin routes file

--- server/app.js
+++ server/app.js
@@ -1,13 +1,19 @@
+const cookieParser = require('cookie-parser');
 import express from 'express';
 import morgan from 'morgan';
 import winston from './config/winston';
-import routes from './routes/routes';
+import routerV1 from './routes/v1/routes';
+import adminRoutesV1 from './routes/v1/adminRoutes';
+
 require('dotenv').config();
 require('./models');
 const app = express();
 
 app.use(morgan('combined', { stream: winston.stream }));
-app.use(routes);
+app.use(cookieParser());
+
+app.use('/api/v1', routerV1);
+app.use('/api/v1/admin', adminRoutesV1);
 
 const server = app.listen(
     process.env.PORT,
